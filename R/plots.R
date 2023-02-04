be745c6fbaa0df2df82e2da4b3711c7380d097ba
marcpires <marcpiresrj@gmail.com>
2023-02-04 08:20:24 -0300

    fix(plot) : Add title

--- R/plots.R
+++ R/plots.R
@@ -1,7 +1,7 @@
 library(plotly)
 library(ggrepel)
 
-scatter_plot <- function(dataset, point_x, point_y , label_x, label_y, save = T, plot_file, plot_path){
+scatter_plot <- function(dataset, point_x, point_y , label_x, label_y, save = T, plot_title, plot_file, plot_path){
   dataset %>%
     ggplot() +
     geom_point(aes(x = point_x, y = point_x),
@@ -14,7 +14,8 @@ scatter_plot <- function(dataset, point_x, point_y , label_x, label_y, save = T,
               se = FALSE,
               size = 1.3) +
     labs(x = label_x,
-        y = label_y) +
+        y = label_y,
+        title = paste(plot_title)) +
     theme_bw()
 }
 
