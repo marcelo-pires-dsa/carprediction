8514c9e75f56611262b6c540a178b22c3894feff
marcpires <marcpiresrj@gmail.com>
2023-07-24 12:06:16 -0300

    Chore: Add version check function

--- R/helpers.R
+++ R/helpers.R
@@ -8,6 +8,14 @@
 library(base)
 library(kableExtra)
 
+#' Check if R version is equal to specified version string
+#' @param version
+#' @return TRUE or FALSE
+is_r_version <- function(version) {
+  return(R.Version()$version.string %>%
+    str_extract("\\d+.\\d+.\\d+") == version)
+}
+
 #TODO: Not working when called from analysis.R
 print_table <- function(dataset) {
     dataset %>%
