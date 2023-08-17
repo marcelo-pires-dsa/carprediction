881b6b72293b80ba3f18ff76b2b6a4c27dc4442a
Marcelo da Silva Pires <136385158+marcelo-pires-dsa@users.noreply.github.com>
2023-08-17 20:55:52 -0300

    Create neptune.py

---
+++ neptune.py
@@ -0,0 +1,16 @@
+import neptune
+
+run = neptune.init_run(
+    api_token=os.getenv("NEPTUNE_API_TOKEN"),
+    project="project='marcpires--dsa/car-price-prediction",
+)
+
+
+PARAMS = {
+    "batch_size": 64,
+    "dropout": 0.2,
+    "learning_rate": 0.001,
+    "optimizer": "Adam",
+}
+
+run["parameters"] = PARAMS
