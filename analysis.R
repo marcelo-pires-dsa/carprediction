b9b285e8541a0871fa8f18daa7c1e48471a386a5
marcelo-pires-dsa <marcelopires@usp.br>
2023-09-08 18:36:56 -0300

    chore(modeling): Add tidymodels

--- analysis.R
+++ analysis.R
@@ -1,14 +1,36 @@
 library(tidyverse)
 library(ggcorrplot)
+library(neptune)
+library(tidymodels)
+
+# Handles package conflicts
+tidymodels_prefer(quiet = F)
 
 source("./R/helpers.R")
 source("./R/plots.R")
 
+run <- neptune_init(
+  project="marcpires--dsa/car-price-prediction",
+  api_token=Sys.getenv("NEPTUNE_API_TOKEN"),
+  name = "Data Analysis",
+  description = "Project aims to predict a car give, using regression models",
+  capture_hardware_metrics = T,
+  capture_stderr = T,
+  capture_stdout = T,
+  capture_traceback = T)
+
+
 # Get arguments from command line
 args <- commandArgs(trailingOnly = T)
+dataset <- "./data/cardetails-clean.csv"
+
+# Track dataset
+neptune_track_files(run["artifacts/datasets"], dataset)
+
+neptune_add(run["sys/tags"], ["lm", "testing"])
 
 # load dataset
-car_dataset <- read.csv("./data/cardetails-clean.csv")
+car_dataset <- read.csv(dataset)
 
 features <- c("year", "kilometer","price")
 
@@ -19,7 +41,14 @@ selected_features <- car_dataset %>%
 corr <- cor(selected_features)
 
 # Exibe a matrix de correçalação
-ggcorrplot(corr)
+corr %>% ggcorrplot(title = "Correlation plot",
+           lab = T, ggtheme = "theme_classic")
+
+selected_features_summary <-  summary(selected_features)
+capture.output(selected_features_summary, file = "./output/dataset_summary")
+
+# Upload plot. Will be converted to a file
+neptune_upload(run["evaluation/correlation"], corr)
 
 # Dataset structure
 str(car_dataset)
@@ -54,6 +83,38 @@ pricekm_plot <- scatter_plot(car_dataset,point_x = car_dataset$kilometer,
                            plot_file = "price_x_kilometer.png",
                            plot_path = "./plots/")
 
+neptune_upload(run["evaluation/scatter-plot"], pricekm_plot)
+
 
 ggsave(filename = "car_price_y_distribution.png", plot = pricekm_plot,
        path = "./plots/", device = "png")
+
+#TODO: Move to a new file after testing
+
+#Estimate an lm (Ordinary Linear Regression) model (price ~ year)
+lm_price_model <- 
+    linear_red() %>% 
+      set_engine("lm")
+
+lm_price_fit <- lm_price_model %>% fit(price ~ year, data = selected_features)
+
+lm_price_fit
+  
+plot(lm_price_fit)
+
+anova(lm_price_fit)
+
+# Model summary
+summary(lm_price_fit)
+
+#run["evaluation/accurary"]
+#run["evaluation/accurary"]
+
+# Predict
+
+new_values <- data.frame(year = 2023)
+predict(lm_price_fit,new_values)
+
+
+
+neptune_upload(run["model"], "output/models/lm.rds")
